25b6bbc36f79d757593d97cf9700a00bdcba31bf
Dylan Ravel <dylanm.ravel@gmail.com>
2024-04-23 17:30:27 -0700

    Testing `ScratchWork.java`

--- ScratchWork.java
+++ ScratchWork.java
@@ -3,5 +3,8 @@ import java.util.LinkedList;
 public class ScratchWork {
     public static void main(String[] args) {
         LinkedList<String> test = new LinkedList<String>();
+        test.add("Hello World");
+
+        System.out.println(test);
     }
 }
